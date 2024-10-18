7eab3324f8ae372a943f10157d278c20baf463f1
Ishwarivijayjagtap <ishwarijagtap20701@gmail.com>
2024-10-18 14:34:04 +0530

    Add project files.

---
+++ PattarnProgram/Pattarn2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PattarnProgram
+{
+    internal class Pattarn2
+    {
+        /*static void Main(string[] args)
+        {
+            for (int rows = 1; rows <6; rows++)
+            {
+                for (int col = 1; col <=rows; col++)
+                {
+                    Console.Write(col);
+                }
+                Console.WriteLine();
+            }
+            for (int rows = 4; rows >=0; rows--)
+            {
+                for (int col = 1; col <= rows; col++)
+                {
+                    Console.Write(col);
+                }
+                Console.WriteLine();
+            }
+        }*/
+    }
+}
---
+++ PattarnProgram/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PattarnProgram
+{
+    internal class Pattarn1
+    {
+       /* static void Main(string[] args)
+        {
+           // Console.WriteLine("Pattarn 1");
+            for (int i = 5; i >= 1; i--)
+            {
+                for (int j = i; j >=1; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }*/
+    }
+}
